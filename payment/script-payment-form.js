0eec6ef179c936d64249d23c59b87179588dfcc3
a_hilan <anastasiia.hileta.kn.2021@lpnu.ua>
2023-05-23 22:09:42 +0300

    checkbox

--- payment/script-payment-form.js
+++ payment/script-payment-form.js
@@ -2,43 +2,88 @@ const emailField = document.querySelector('.Email input[name="email"]');
 const hideEmailBtn = document.querySelector('.Email input[type="submit"]');
 
 hideEmailBtn.addEventListener('click', function () {
-  const email = emailField.value; // Отримуємо значення введеної електронної пошти
-  
-  // Перевіряємо, чи введено значення електронної пошти
-  if (email) {
-    emailField.style.display = 'none';
-    hideEmailBtn.style.display = 'none';
-    // Виводимо повідомлення з підтвердженням
-  alert(`Вашу пошту (${email}) успішно записано. Дякуємо!`);
-  } else {
-    alert("Введіть пошту!");
-  }
+    const email = emailField.value; // Отримуємо значення введеної електронної пошти
+
+    // Перевіряємо, чи введено значення електронної пошти
+    if (email) {
+        emailField.style.display = 'none';
+        hideEmailBtn.style.display = 'none';
+        // Виводимо повідомлення з підтвердженням
+        alert(`Вашу пошту (${email}) успішно записано. Дякуємо!`);
+    } else {
+        alert("Введіть пошту!");
+    }
 });
+
+
+
+// ---------------------- PRODUCT INFO ----------------------
 
 
 
 function getProductDetails(productId) {
     return fetch('products.json')
-      .then(response => response.json())
-      .then(products => {
-        const parsedProductId = parseInt(productId, 10);
-        const product = products.find(item => item.id === parsedProductId);
-  
-        if (product) {
-          const productIdField = document.getElementById('product-id');
-          const productCostField = document.getElementById('product-cost');
-  
-          productIdField.value = product.id;
-          productCostField.value = product.price;
-        } else {
-          alert(`Product not found! ${parsedProductId}`);
-        }
-      })
-      .catch(error => console.error(error));
+        .then(response => response.json())
+        .then(products => {
+            const parsedProductId = parseInt(productId, 10);
+            const product = products.find(item => item.id === parsedProductId);
+
+            if (product) {
+                const productIdField = document.getElementById('product-id');
+                const productCostField = document.getElementById('product-cost');
+
+                productIdField.value = product.id;
+                productCostField.value = product.price;
+            } else {
+                alert(`Product not found! ${parsedProductId}`);
+            }
+        })
+        .catch(error => console.error(error));
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+
+getProductDetails(id);
+
+
+
+// ---------------------- CHECBOX DELIVERY METHOD ----------------------
+
+
+
+const fedexCheckbox = document.getElementById('FedEx');
+const dhlCheckbox = document.getElementById('DHL');
+
+fedexCheckbox.addEventListener('change', function () {
+    if (fedexCheckbox.checked) {
+        dhlCheckbox.checked = false;
+    }
+});
+
+dhlCheckbox.addEventListener('change', function () {
+    if (dhlCheckbox.checked) {
+        fedexCheckbox.checked = false;
+    }
+});
+
+
+
+// ---------------------- CHECBOX PAY METHOD ----------------------
+
+
+
+const paypalCheckbox = document.getElementById('PayPal');
+const bitcoinCheckbox = document.getElementById('Bitcoin');
+
+paypalCheckbox.addEventListener('change', function() {
+  if (paypalCheckbox.checked) {
+    bitcoinCheckbox.checked = false;
   }
-  
-  const urlParams = new URLSearchParams(window.location.search);
-  const id = urlParams.get('id');
-  
-  getProductDetails(id);
-  
+});
+
+bitcoinCheckbox.addEventListener('change', function() {
+  if (bitcoinCheckbox.checked) {
+    paypalCheckbox.checked = false;
+  }
+});
